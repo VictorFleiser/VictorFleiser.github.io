ead1168feafb6c612f8de331af534ed5f4ea756d
FLEISER Victor <64595232+VictorFleiser@users.noreply.github.com>
2025-10-12 19:51:28 +0200

    links in description

--- script.js
+++ script.js
@@ -2,12 +2,20 @@ fetch('projects.json')
   .then(res => res.json())
   .then(projects => {
     const container = document.querySelector('.projects-grid');
+
+    // Convert [text](url) and line breaks
+    const parseMarkdownBasics = (text) => {
+      return text
+        .replace(/\[([^\]]+)\]\(([^)]+)\)/g, '<a href="$2" target="_blank">$1</a>') // links
+        .replace(/\n/g, '<br>'); // new lines
+    };
+
     container.innerHTML = projects.map(p => `
       <div class="project">
         <img src="${p.image}" alt="${p.title}">
         <div class="overlay">
           <h2>${p.title}</h2>
-          <p>${p.description}</p>
+          <p>${parseMarkdownBasics(p.description)}</p>
           <a href="${p.link}" target="_blank">View Repo</a>
         </div>
       </div>
